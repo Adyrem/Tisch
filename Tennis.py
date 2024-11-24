2c6e6a7602edc1da18b34b3bbf286a5e56d2a83b
Adyrem <fanfohcd@gmail.com>
2024-11-24 14:57:24 +0100

    Added basic functionality for spin

--- Tennis.py
+++ Tennis.py
@@ -7,6 +7,8 @@ server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 server_socket.bind(('', int(sys.argv[1])))
 
 while True:
-    # TODO add parsing, error handling and data transformation
+    # TODO add error handling
     message, address = server_socket.recvfrom(1024)
+
+    message = (int.from_bytes(message, 'big') + 1).to_bytes(2, 'big')
     server_socket.sendto(message, address)
--- Tisch.py
+++ Tisch.py
@@ -1,19 +1,23 @@
 import socket
 import sys
 
+spin = 1
+
 for pings in range(10):
     client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     client_socket.settimeout(1.0)
-    # TODO change payload to actual data
-    message = b'test'
+    message = spin.to_bytes(2, 'big')
 
     # TODO add default value in case no argument is passed
     # TODO change to named argument instead
     addr = (sys.argv[1], int(sys.argv[2]))
 
+    print(f"sending spin : {spin}")
     client_socket.sendto(message, addr)
     try:
         data, server = client_socket.recvfrom(1024)
-        print(f'{data} {pings}')
+        spin = int.from_bytes(data, 'big')
+        print(f"received spin : {spin}")
+        spin += 1
     except socket.timeout:
         print('REQUEST TIMED OUT')
