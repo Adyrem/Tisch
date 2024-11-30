6b7e5ef0fa36453a6ea219f64ece036eeba68595
Adyrem <fanfohcd@gmail.com>
2024-11-30 10:24:12 +0100

    Added error handling, named arguments and defaults

--- Tennis.py
+++ Tennis.py
@@ -1,14 +1,31 @@
 import socket
 import sys
+import argparse
+import random
+
+parser=argparse.ArgumentParser()
+parser.add_argument("-p", help="Port to listen on")
+parser.add_argument("-d", help="Randomly drops one in N packages to test error handling")
+args=parser.parse_args()
+
+port = 4456
+drop = 0
+if args.p:
+    port = int(args.p)
+if args.d:
+    drop = int(args.d)
 
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-# TODO add default value in case no argument is passed
-# TODO change to named argument instead
-server_socket.bind(('', int(sys.argv[1])))
+server_socket.bind(('', port))
 
 while True:
-    # TODO add error handling
-    message, address = server_socket.recvfrom(1024)
-
-    message = (int.from_bytes(message, 'big') + 1).to_bytes(2, 'big')
-    server_socket.sendto(message, address)
+    try:
+        message, address = server_socket.recvfrom(1024)
+        message = (int.from_bytes(message, 'big') + 1).to_bytes(2, 'big')
+        if (drop > 0):
+            if random.randint(0, drop) != 0:
+                server_socket.sendto(message, address)
+        else:
+            server_socket.sendto(message, address)
+    except Exception as e:
+        print(f"{e}")
--- Tisch.py
+++ Tisch.py
@@ -1,23 +1,35 @@
 import socket
 import sys
+import argparse
 
+parser=argparse.ArgumentParser()
+parser.add_argument("-i", help="IP to ping")
+parser.add_argument("-p", help="Port to ping")
+args=parser.parse_args()
+
+ip = args.i
+port = int(args.p)
 spin = 1
 
-for pings in range(10):
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    client_socket.settimeout(1.0)
-    message = spin.to_bytes(2, 'big')
+if ip == "":
+    ip = "127.0.0.1"
+if port == 0:
+    port = 4456
 
-    # TODO add default value in case no argument is passed
-    # TODO change to named argument instead
-    addr = (sys.argv[1], int(sys.argv[2]))
+client_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+client_socket.settimeout(1.0)
+addr = (ip, port)
 
-    print(f"sending spin : {spin}")
-    client_socket.sendto(message, addr)
+for attempts in range(4):
     try:
+        print(f"sending spin : {spin}")
+        message = spin.to_bytes(2, 'big')
+        client_socket.sendto(message, addr)
         data, server = client_socket.recvfrom(1024)
         spin = int.from_bytes(data, 'big')
         print(f"received spin : {spin}")
         spin += 1
     except socket.timeout:
-        print('REQUEST TIMED OUT')
+        print("REQUEST TIMED OUT")
+    except Exception as e:
+        print(f"{e}")
